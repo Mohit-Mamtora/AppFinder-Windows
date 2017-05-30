12dd63af303fda7cad88da9c3907c19f9d6a4663
Mohit-Mamtora <mamtora.mohit506@gmail.com>
2017-05-31 00:55:01 +0530

    Create OnMyAppFind.java

---
+++ AppFinderWindows1.0/OnMyAppFind.java
@@ -0,0 +1,9 @@
+package mohit;
+
+
+public interface OnMyAppFind {
+	void Onresponce(String name,String ip);
+	void OnSignalSendedTo(String GateWayIp);
+	void OnListioning();
+	void OnListioningStop();
+}
